2e408c3d298e478988248431b27670225efd589f
ErikKassubek <kassubek.erik@gmail.com>
2025-06-12 10:19:09 +0200

    add fuzzing examples to doc

---
+++ doc/fuzzing/examples/GFuzz/selectWithDefault.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+// The following code is an example in which GFuzz is helpful.
+// It consists of a select with one case an a default.
+// The case (triggered by the send on c) calls code which contains a send on closed channel bug.
+// Let's assume, that it is very likely, but not guaranteed, that the
+// select, and therefore the default case is executed before the send on c.
+// In a dynamic analysis, we can only analyze program parts that are actually run.
+// Since in this program executing the default is much more likely, the dynamic
+// analysis will probably not be able to detect the unlikely, but still possible
+// bug that arises when the c case is triggered.
+// By forcing the program to execute the c case as the preferred case,
+// GFuzz is able to aid in detecting this bug.
+
+func TestSelectWithDefault(_ *testing.T) {
+	c := make(chan int)
+
+	go func() {
+		// some code
+		time.Sleep(300 * time.Millisecond)
+		c <- 1
+	}()
+
+	select {
+	case <-c:
+		panic("CODE WITH PANIC")
+	default:
+	}
+}
---
+++ doc/fuzzing/examples/goPie/replay.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+// This program is an example, of how the partial replay in GoPie
+// may miss the execution of the mutated section, while the partially full replay
+// in GoPie+ and GoPieHB can avoid this.
+// Lets assume in the recorded run, the TryLock was executed before to Lock,
+// therefore being able to acquire the mutex. Let's also assume, that the
+// mutated code position is fully in the mutatedCode function.
+// If we let the code run freely and only enforce the order in the mutated
+// section, it is possible that in execution of this run, the Lock happens
+// before the TryLock and the TryLock is not able to acquire the lock.
+// In this case, the program would never get to the relevant, mutated section.
+// By enforcing the exact order of the code before the relevant section,
+// like it is done in GoPie+ and GoPieHB, we can avoid this, since the replay
+// mechanism makes sure, that the Lock and TryLock are executed in the
+// correct order.
+
+func TestGoPieReplay(_ *testing.T) {
+	m := sync.Mutex{}
+
+	go func() {
+		m.Lock()
+		// some code
+		time.Sleep(time.Second)
+		m.Unlock()
+	}()
+
+	res := m.TryLock()
+	if res {
+		mutatedCode()
+		m.Unlock()
+	}
+
+}

5ec4fe17ad0345d212a1b3ca2f281d50d7e4d9d8
ErikKassubek <kassubek.erik@gmail.com>
2025-08-09 16:55:58 +0200

    fix small bug

--- advocate/results/stats/statsFuzzing.go
+++ advocate/results/stats/statsFuzzing.go
@@ -35,7 +35,7 @@ func CreateStatsFuzzing(pathFolder, progName string) error {
 	resultPath := filepath.Join(pathFolder, "advocateResult")
 	statsAnalyzerPath := filepath.Join(resultPath, "statsAnalysis_"+progName+".csv")
 	statsFuzzingPath := filepath.Join(resultPath, "statsFuzzing_"+progName+".csv")
-	statsFuzzPath := filepath.Join(resultPath, "statsFuzzing_"+progName+".csv")
+	statsFuzzPath := filepath.Join(resultPath, "statsFuzz_"+progName+".csv")
 
 	log.Info("Create fuzzing statistics")
 
